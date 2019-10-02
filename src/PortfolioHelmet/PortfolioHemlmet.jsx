1a69c26bccfaee8545e62e5efb560bb5e1c15e8f
Christian Lagasca <cjrl998@icloud.com>
2019-10-02 11:34:00 -0400

    updated Helmet

--- src/PortfolioHelmet/PortfolioHemlmet.jsx
+++ src/PortfolioHelmet/PortfolioHemlmet.jsx
@@ -6,6 +6,9 @@ class PortfolioHelmet extends React.Component {
         return(
                 <Helmet>
                     <title>Software Developer - Christian Lagasca</title>
+                    <meta name="description" content="Christian Lagasca Personal Website"/>
+                    <meta name="keywords" content="Christian Lagasca,Software Developer,Front-End,Full-Stack"/>
+                    <meta name="author" content="Christian Lagasca"/>
                     <link href="./src/fontawesome-free-5.10.1-web/css/all.css" rel="stylesheet"/>
                     <script src="https://kit.fontawesome.com/bd54b583dc.js"></script>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css"/>
