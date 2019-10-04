c6d3b36bb1dd184708757037b1748b44b0441e99
Christian Lagasca <cjrl998@icloud.com>
2019-10-04 14:32:15 -0400

    added a minimal CN tower picture

--- src/home-page/home-page.jsx
+++ src/home-page/home-page.jsx
@@ -1,36 +1,33 @@
 import React from 'react';
 import ReactRotatingText from 'react-rotating-text';
-
 import './home-page.css';
 import '../css/main.css'
-
+import CnTowerSkyline from '../images/svg/CnTowerSkyline.svg';
 class HomePage extends React.Component {
-
-
     render() {
         return (
-            
+
             <div className="home-container">
-                    <div className="home-text-wrapper">
-                        <h1>Hello.</h1>
-                        <h1>I'm Chris</h1>
-                        <h2>a <span> Software Developer</span></h2>
-                        <h2>based in <span> Toronto</span></h2>
-                        <h2>who loves 
+    <div className="home-text-wrapper">
+        <h1>Hello.</h1>
+        <h1>I'm Chris</h1>
+        <h2>a <span> Software Developer</span></h2>
+        <h2>based in <span> Toronto</span></h2>
+        <h2>who loves 
                             <span>     
-                                <ReactRotatingText items={[' Art',' Design', ' Solving problems', ' Collaborative work', ' Curating playlists', ' Concerts']} />
+                                <ReactRotatingText items={[' Art',' Design',' Minimalism', ' Solving problems', ' Collaborative work', ' Curating playlists', ' Concerts']} />
                              </span>
                         </h2>
-                        <div className="alert-card fade-in">
-                            <i className="fas fa-exclamation-circle"></i>
-                            <p>Redesigning the site to showcase my Bootcamp journey over at BrainStation !</p>
-                        </div>
-                    </div>
-                    <div>
-                    </div>
-            </div>
+        <div className="alert-card fade-in">
+            <i className="fas fa-exclamation-circle"></i>
+            <p>Redesigning the site to showcase my Bootcamp journey over at BrainStation !</p>
+        </div>
+    </div>
+    <div className="image-container">
+        <img src={CnTowerSkyline}></img>
+    </div>
+</div>
         );
     }
 }
-
 export default HomePage;
