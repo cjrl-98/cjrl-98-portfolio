ec4ce17b72b905ce0ca208101314891db98c71e1
Christian Lagasca <cjrl998@icloud.com>
2019-10-04 11:46:26 -0400

    finished adding typewritter effect

--- src/home-page/home-page.jsx
+++ src/home-page/home-page.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import ReactRotatingText from 'react-rotating-text';
 
 import './home-page.css';
 import '../css/main.css'
 
 class HomePage extends React.Component {
+
+
     render() {
         return (
+            
             <div className="home-container">
                     <div className="home-text-wrapper">
                         <h1>Hello.</h1>
                         <h1>I'm Chris</h1>
                         <h2>a <span> Software Developer</span></h2>
                         <h2>based in <span> Toronto</span></h2>
-                        <h2>who loves purposeful <span> Designs</span></h2>
-                        
+                        <h2>who loves 
+                            <span>     
+                                <ReactRotatingText items={[' Design', ' Solving problems', ' Collaborative work', ' Curating playlists', ' Concerts']} />
+                             </span>
+                        </h2>
                         <div className="alert-card">
                             <i class="fas fa-exclamation-circle"></i>
                             <p>Redesigning the site to showcase my Bootcamp journey over at BrainStation !</p>
