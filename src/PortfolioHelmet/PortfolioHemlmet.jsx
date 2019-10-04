681cd8cd0ff254ed179d582fcf8667ca801430cd
Christian Lagasca <cjrl998@icloud.com>
2019-10-04 16:50:59 -0400

    trying seo opt

--- src/PortfolioHelmet/PortfolioHemlmet.jsx
+++ src/PortfolioHelmet/PortfolioHemlmet.jsx
@@ -7,7 +7,7 @@ class PortfolioHelmet extends React.Component {
                 <Helmet>
                     <title>Software Developer - Christian Lagasca</title>
                     <meta name="description" content="Christian Lagasca Personal Website"/>
-                    <meta name="keywords" content="Christian Lagasca,Software Developer,Front-End,Full-Stack"/>
+                    <meta name="keywords" content="Christian Lagasca,christian lagasca,toronto,Software Developer,Front-End,Full-Stack,Software Developer - Christian Lagasca"/>
                     <meta name="author" content="Christian Lagasca"/>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css"/>
                     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet"/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactGA from 'react-ga';
+
 
 
 import "../css/reset.css";
@@ -9,14 +9,8 @@ import "../css/animations.scss";
 import PortfolioHelmet from "../PortfolioHelmet/PortfolioHemlmet";
 import LayoutContainer from "../layout-container/layout-container";
 
-function initializeReactGA() {
-  ReactGA.initialize('UA-123791717-1');
-  ReactGA.pageview('/homepage');
-}
-
 const IndexPage = () => (
   <React.Fragment>
-      <initializeReactGA/>
       <PortfolioHelmet/> 
       <LayoutContainer/>
   </React.Fragment>
