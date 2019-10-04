3bb533cd22d5aea5c8752e092ff0f49f04daa791
Christian Lagasca <cjrl998@icloud.com>
2019-10-04 16:13:10 -0400

    added analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react";
+import ReactGA from 'react-ga';
+
 
 import "../css/reset.css";
 import "../css/main.css";
 import "../css/animations.scss";
 
-
 import PortfolioHelmet from "../PortfolioHelmet/PortfolioHemlmet";
 import LayoutContainer from "../layout-container/layout-container";
 
+function initializeReactGA() {
+  ReactGA.initialize('UA-123791717-1');
+  ReactGA.pageview('/homepage');
+}
 
 const IndexPage = () => (
   <React.Fragment>
+      <initializeReactGA/>
       <PortfolioHelmet/> 
       <LayoutContainer/>
   </React.Fragment>
