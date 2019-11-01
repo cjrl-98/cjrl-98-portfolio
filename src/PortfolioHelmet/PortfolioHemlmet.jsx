ddf68d6f9bc034a978471f2039c5f11c919e986d
Christian Lagasca <cjrl998@icloud.com>
2019-11-01 14:30:37 -0400

    testing new og image display

--- src/PortfolioHelmet/PortfolioHemlmet.jsx
+++ src/PortfolioHelmet/PortfolioHemlmet.jsx
@@ -11,7 +11,7 @@ class PortfolioHelmet extends React.Component {
                     <meta name="description" content="Christian Lagasca Personal Website"/>
                     <meta name="keywords" content="Christian Lagasca,christian lagasca,toronto,Software Developer,Front-End,Full-Stack,Software Developer - Christian Lagasca"/>
                     <meta name="author" content="Christian Lagasca"/>
-                    <meta property="og:image" content={cjrlLogo}></meta>
+                    <meta property="og:image" content="https://images.unsplash.com/photo-1560155069-ad79768f2666?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80gi"></meta>
                     <meta property="og:site_name" content="Software Developer - Christian Lagasca"/>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/css/swiper.min.css"/>
                     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet"/>
