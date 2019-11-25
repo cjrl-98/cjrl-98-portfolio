a1d9daaf0eafa4faf6ed53ff546dd0f900a4bc5d
Christian <cjrl998@icloud.com>
2019-11-25 03:11:26 -0500

    Update index.js
    updated animation timings

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
       <BackgroundEffect/>
       <BackgroundEffect/>
     </div>
-    <motion.div initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale: 1}} transition={{ type: "spring", stiffness: 100, delay: 1.5 }}>
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{ type: "spring", stiffness: 100, delay: 0.5 }}>
       <LayoutContainer/>
     </motion.div>
     <footer className="footer">
